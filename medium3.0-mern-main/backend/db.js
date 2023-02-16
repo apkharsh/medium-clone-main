f76a712e91ba7b79528dfaae08069b68d7e25365
apkharsh <apkharsh@gmail.com>
2023-02-16 22:33:51 +0530

    starting files for medium clone

---
+++ medium3.0-mern-main/backend/db.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+// mongo db localhost url
+
+const url = "mongodb://localhost:27017/medium";
+module.exports.connect = () => {
+  mongoose
+    .connect(url, {
+      useNewUrlParser: true,
+      // useFindAndModify: false,
+      useUnifiedTopology: true,
+      // useCreateIndex: true,
+    })
+    .then(() => console.log("MongoDB is connected successfully"))
+    .catch((err) => console.log("Error: ", err));
+};
